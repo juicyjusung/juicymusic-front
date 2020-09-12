73c5353e7d4bb349c8046c8e6b1c7e9a6a550ac7
juicyjusung <juicyjusung@gmail.com>
2020-09-13 02:20:23 +0900

    feat: Login Form Componet

---
+++ src/components/user/LoginForm.vue
@@ -0,0 +1,99 @@
+<template>
+  <v-card class="elevation-6 ma-auto rounded-lg" width="400px" min-width="300px" ref="form">
+    <v-card-title class="d-flex justify-center py-6">
+      juicymusic
+    </v-card-title>
+    <v-card-text class="d-flex flex-column">
+      <v-text-field
+        v-model="loginInput.userId"
+        ref="userId"
+        class="ma-2"
+        prepend-icon="fas fa-user-tag"
+        :rules="rules.useridRules"
+        label="ID"
+        required
+      ></v-text-field>
+      <v-text-field
+        v-model="loginInput.password"
+        ref="password"
+        class="ma-2 input-group--focused"
+        prepend-icon="fas fa-lock"
+        :rules="rules.min"
+        :append-icon="showPassword ? 'fas fa-eye-slash' : 'fas fa-eye'"
+        :type="showPassword ? 'text' : 'password'"
+        name="password"
+        label="Password"
+        required
+        @keyup.native.enter="submit()"
+        @click:append="showPassword = !showPassword"
+      ></v-text-field>
+    </v-card-text>
+    <v-card-actions class="d-flex align-center justify-center ">
+      <v-btn class="ma-2" color="primary" @click.stop="submit">Login</v-btn>
+      <v-btn class="mx-2" color="secondary" @click.stop="onOpenSignup">Signup</v-btn>
+    </v-card-actions>
+  </v-card>
+</template>
+<script lang="ts">
+import { Component, Emit, Vue } from 'vue-property-decorator';
+import { LoginInput, User } from '@/types/User';
+import { VForm } from '@/types/Common';
+import { getKeyValue } from '@/utils/CommonUtils';
+import { Action } from 'vuex-class';
+import { AxiosResponse } from 'axios';
+import { ResponseBody } from '@/types/Reponse';
+
+@Component({
+  components: {},
+})
+export default class LoginForm extends Vue {
+  @Emit('onOpenSignup') private onOpenSignup() {}
+  @Action('login', { namespace: 'auth' }) private login!: (user: LoginInput) => ResponseBody;
+  /******************************************************************
+   * Variable
+   * ****************************************************************/
+  private showPassword = false;
+  private loginInput: LoginInput = { userId: '', password: '' };
+  private formHasErrors = false;
+  private rules = {
+    min: [(v: string) => v.length >= 8 || 'Min 8 characters'],
+    useridRules: [(v: string) => !!v || 'ID is required'],
+  };
+  get form() {
+    return this.loginInput;
+  }
+
+  /**************************************************************************
+   * Methods
+   * ***********************************************************************/
+  protected async submit() {
+    this.formHasErrors = false;
+    Object.keys(this.form).forEach((key: string) => {
+      if (!getKeyValue(key)(this.form)) this.formHasErrors = true;
+      const form = this.$refs[key] as VForm;
+      form.validate(true);
+    });
+    if (this.formHasErrors) return;
+    try {
+      const body = await await this.login(this.loginInput);
+      console.log('%c [JL] submit - body', 'font-size: 16px; color:  red;', body);
+      if (body.message) {
+        this.$notify({
+          group: 'all',
+          type: 'error',
+          text: body.message,
+        });
+      } else {
+        this.$notify({
+          group: 'all',
+          type: 'success',
+          text: '로그인이 완료되었습니다.',
+        });
+        await this.$router.push('/');
+      }
+    } catch (e) {
+      console.error(e);
+    }
+  }
+}
+</script>
---
+++ src/views/Login.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-container class="fill-height align-center justify-center">
+    <LoginForm @onOpenSignup="() => (signupOverlay = true)"></LoginForm>
+    <v-overlay :value="signupOverlay" light :opacity="0.9">
+      <SignupForm @onClose="() => (signupOverlay = false)"> </SignupForm>
+    </v-overlay>
+  </v-container>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+import LoginForm from '@/components/user/LoginForm.vue';
+import SignupForm from '@/components/user/SignupForm.vue';
+
+@Component({
+  components: { SignupForm, LoginForm },
+})
+export default class Login extends Vue {
+  /******************************************************************
+   * Variable
+   * ****************************************************************/
+  private signupOverlay = false;
+
+  /**************************************************************************
+   * Methods
+   * ***********************************************************************/
+}
+</script>
